7c2a2dd5983570f73deb6f39833a7ca41f1cfe68
aaddaamm30 <a.daman.loo@gmail.com>
2017-11-05 04:24:43 -0500

    works cashmeousidehowboudat

--- hello-gm/src/js/app.js
+++ hello-gm/src/js/app.js
@@ -3,7 +3,6 @@
 //get the lat long and return tuple
 function mainWeather(position){
   //var position = gm.info.getCurrentPosition();
-  console.log(position);
   var lat = position.coords.latitude;
   var lng = position.coords.longitude;
   getPlaceKey(formatURL(lat, lng));
@@ -11,28 +10,44 @@ function mainWeather(position){
 
 //get the actual weather data
 function pullWeather(key){
-
   var newClient = function(){
     this.get = function(address, callback){
       var nextRequest = new XMLHttpRequest();
       nextRequest.onreadystatechange = function(){
-        if(newRequest.readyState == 4 && newRequest.status == 200){
-          callback(newRequest.responseText);
+        if(nextRequest.readyState == 4 && nextRequest.status == 200){
+          callback(nextRequest.responseText);
         }
       }
       nextRequest.open("GET", address, true);
       nextRequest.send(null);
     }
   }
+
+  //set up new http call to get local weather data
+  var newrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  newrl = newrl.concat(key);
+  newrl = newrl.concat('?apikey=HackPSU2017');
+  var goget = new newClient;
+  goget.get(newrl, function(response){
+    var currentSky = JSON.parse(response);
+    var lblDiv = document.getElementById('weatherInfo');
+    var newString = 'Its Fucking ';
+    console.log(currentSky);
+    console.log(currentSky[0].WeatherText);
+    newString = newString.concat(currentSky[0].WeatherText);
+    lblDiv.innerHTML = newString;
+  });
 }
+
+
 //function that formats url
 function formatURL(lat,loc){
 
   var url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=HackPSU2017&q=';
-  var url1 = url.concat(lat);
-  var url2 = url1.concat('%2C%20');
-  var url3 = url2.concat(loc);
-  return(url3);
+  var url = url.concat(lat);
+  var url = url.concat('%2C%20');
+  var url = url.concat(loc);
+  return(url);
 }
 
 //get the key value
@@ -40,11 +55,11 @@ function getPlaceKey(theURL){
 
   //set callback
   var HttpClient = function() {
-    this.get = function(aUrl, Callback){
+    this.get = function(aUrl, callback){
       var newRequest = new XMLHttpRequest();
       newRequest.onreadystatechange = function(){
         if(newRequest.readyState == 4 && newRequest.status == 200){
-          Callback(newRequest.responseText);
+          callback(newRequest.responseText);
         }
       }
       newRequest.open("GET", aUrl, true);
@@ -55,9 +70,8 @@ function getPlaceKey(theURL){
   //use callback defined above
   var returnKey = new HttpClient();
   returnKey.get(theURL, function(response){
-    console.log(response);
     var defd = JSON.parse(response);
-    console.log(defd.Key);
+    pullWeather(defd.Key);
   });
 }
 
